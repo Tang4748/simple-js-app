fe58f922611b6336ae3815d3502e3da6e3485784
Tang4748 <109551346+Tang4748@users.noreply.github.com>
2022-12-20 01:32:22 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ let favoriteFood = 'Pizza';
 document.write(favoriteFood);
 
 const greetings =
-          ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+          ['Hello world!','Howdy Yall!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
       // Add a random greeting to the page.
       function addRandomGreeting() {
