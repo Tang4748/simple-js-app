9db279f7d0b23a715433de5f1122b486b04e969b
Tang4748 <109551346+Tang4748@users.noreply.github.com>
2022-12-24 14:32:28 -0600

    Added pokemonList array
    Added blank array, then added pokemon into array with name, height and type.
    Changed scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
-alert('Hello world');
-
-let favoriteFood = 'Pizza';
-
-document.write(favoriteFood);
-
-const greetings =
-          ['Hello world!','Howdy Yall!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-      // Add a random greeting to the page.
-      function addRandomGreeting() {
-        const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-        const greetingContainer = document.getElementById('greeting-container');
-        greetingContainer.innerText = greeting;
-      }
+let pokemonList = [
+  { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+  { name: 'Charmander', height: 0.6, types: ['fire'] },
+  { name: 'Squirtle', height: 0.5, types: ['water'] },
+  { name: 'Pikachu', height: 0.4, types: ['electric'] },
+  { name: 'Eevee', height: 0.3, types: ['normal'] },
+  { name: "Charizard", height: 1.7, type: ["Fire", "Flying"] },
+  { name: "Mewtwo", height: 2, type: ["Psychic"] },
+  { name: "Blastoise", height: 1.6, type: "Water" },
+  { name: "Pidgeot", height: 1.5, type: ["Normal", "Flying"] },
+  { name: "Venusaur", height: 2, type: ["Grass", "Poison"] },
+  { name: "Mew", height: 0.4, type: "Psychic" },
+  { name: "Moltres", height: 2, type: ["Fire", "Flying"] },
+  { name: "Zapdos", height: 1.6, type: ["Electric", "Flying"] },
+  { name: "Articuno", height: 1.7, type: ["Ice", "Flying"] },
+  { name: "Wartortle", height: 1, type: "Water" },
+];
 
