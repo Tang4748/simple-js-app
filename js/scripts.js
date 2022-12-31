eabc66c3e34c218f1f9d0c853c1e762d8ae7aa64
Tang4748 <109551346+Tang4748@users.noreply.github.com>
2022-12-31 00:59:25 -0600

    Added For Loop to script.js
    added for loop to print name and height to console
    added condition if pokemon height is above 2, print "wow thats big"

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,29 @@ let pokemonList = [
   { name: 'Squirtle', height: 0.5, types: ['water'] },
   { name: 'Pikachu', height: 0.4, types: ['electric'] },
   { name: 'Eevee', height: 0.3, types: ['normal'] },
-  { name: "Charizard", height: 1.7, type: ["Fire", "Flying"] },
-  { name: "Mewtwo", height: 2, type: ["Psychic"] },
-  { name: "Blastoise", height: 1.6, type: "Water" },
-  { name: "Pidgeot", height: 1.5, type: ["Normal", "Flying"] },
-  { name: "Venusaur", height: 2, type: ["Grass", "Poison"] },
-  { name: "Mew", height: 0.4, type: "Psychic" },
-  { name: "Moltres", height: 2, type: ["Fire", "Flying"] },
-  { name: "Zapdos", height: 1.6, type: ["Electric", "Flying"] },
-  { name: "Articuno", height: 1.7, type: ["Ice", "Flying"] },
-  { name: "Wartortle", height: 1, type: "Water" },
+  { name: "Charizard", height: 1.7, type: ['Fire", "Flying'] },
+  { name: "Mewtwo", height: 2.001, type: ['Psychic'] },
+  { name: "Blastoise", height: 1.6, type: 'Water' },
+  { name: "Pidgeot", height: 1.5, type: ['Normal', 'Flying'] },
+  { name: "Venusaur", height: 2, type: ['Grass', 'Poison'] },
+  { name: "Mew", height: 0.4, type: 'Psychic' },
+  { name: "Moltres", height: 2, type: ['Fire', 'Flying'] },
+  { name: "Zapdos", height: 1.6, type: ['Electric', 'Flying'] },
+  { name: "Articuno", height: 1.7, type: ['Ice', 'Flying'] },
+  { name: "Wartortle", height: 1, type: 'Water' },
 ];
 
+/* tell 'for-loop' to look at length of variable array above first */
+for (let i=0; i < pokemonList.length; i++){
+  /* set condition for key-value of objects in array */
+    if (pokemonList[i].height > 2){
+      console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!");
+  /* inserted break line to put each object on a new line  */
+      document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!" + "<br>");
+  /* only need to use 'else' not 'else-if' as we want the output to be on just one object  */
+  } else {
+      console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+  /* inserted break line tag to put each object on a new line  */
+      document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>")
+  }
+  }
