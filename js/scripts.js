dca4287cd7606444f18fe19c5cc0ff91781df962
Tang4748 <109551346+Tang4748@users.noreply.github.com>
2023-01-15 15:29:03 -0600

    added Object.keys to Script.js
    added Object.keys

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,11 @@ let pokemonRepository = (function () {
 
   function add(pokemon) {
     if (
-      typeof pokemon === "object" &&
+      typeof pokemon === "object" && Object.keys(pokemon).includes( 
       "name" in pokemon &&
       "type" in pokemon &&
       "height" in pokemon 
-    ){
+    )){
     pokemonList.push(pokemon);
   } else {
     console.log("pokemon is not correct");
@@ -37,15 +37,15 @@ let pokemonRepository = (function () {
 
 //Add pokemon to the list with the button format
   function addListItem (pokemon) {
-    let pokemonList = document.querySelector('.pokemon-list');
-    let listItem = document.createElement('li');
-    let button = document.createElement('button');
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
     button.innerText = pokemon.name;
-    button.classList.add('button-class')
+    button.classList.add("button-class")
     listItem.appendChild(button);
     pokemonList.appendChild(listItem); 
 //added event listener: returns all pokemon info to console when button is clicked
-    button.addEventListener('click', function(event) {
+    button.addEventListener("click", function(event) {
       showDetails(pokemon);
     });
   }
