47917ab6fef31d62fe236b50f58ce69cd2f67c61
Tang4748 <109551346+Tang4748@users.noreply.github.com>
2023-01-20 22:10:44 -0600

    updated JS
    added promise and fetch files This is a huge update - added 2 new files. Added all of the code from the 1.7 exercise without updating github regularly see it will be a large commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,17 @@
 let pokemonRepository = (function () {
-
-  let pokemonList = [
-    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-    { name: "Charmander", height: 0.6, types: ["fire"] },
-    { name: "Squirtle", height: 0.5, types: ["water"] },
-    { name: "Pikachu", height: 0.4, types: ["electric"] },
-    { name: "Eevee", height: 0.3, types: ["normal"] },
-    { name: "Charizard", height: 1.7, type: ["Fire", "Flying"] },
-    { name: "Mewtwo", height: 2.001, type: ["Psychic"] },
-    { name: "Blastoise", height: 1.6, type: "Water" },
-    { name: "Pidgeot", height: 1.5, type: ["Normal", "Flying"] },
-    { name: "Venusaur", height: 2, type: ["Grass", "Poison"] },
-    { name: "Mew", height: 0.4, type: "Psychic" },
-    { name: "Moltres", height: 2, type: ["Fire", "Flying"] },
-    { name: "Zapdos", height: 1.6, type: ["Electric", "Flying"] },
-    { name: "Articuno", height: 1.7, type: ["Ice", "Flying"] },
-    { name: "Wartortle", height: 1, type: "Water" },
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
     if (
-      typeof pokemon === "object" && Object.keys(pokemon).includes( 
-      "name" in pokemon &&
-      "type" in pokemon &&
-      "height" in pokemon 
-    )){
-    pokemonList.push(pokemon);
-  } else {
-    console.log("pokemon is not correct");
+      typeof pokemon === "object" &&
+      "name" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
   }
-  }
-
   function getAll() {
     return pokemonList;
   }
@@ -50,22 +31,62 @@ let pokemonRepository = (function () {
     });
   }
 
-  function showDetails(pokemon){
-    console.log(pokemon);
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
   }
 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+
+  }
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
-    showDetails: showDetails
+    showDetails: showDetails,
+    loadList: loadList,
+    loadDetails: loadDetails 
   };
 })();
 
 let pokemonList= pokemonRepository.getAll();
 
-pokemonRepository.add({name: "Pikachu", height: 1.1 , type: "fire"});
-
+fetch('/users.json')
+  .then(function(response) {
+    return response.json()
+  }).then(function(json) {
+    console.log('parsed json', json)
+  }).catch(function(ex) {
+    console.log('parsing failed', ex)
+  })
 
 /* tell 'for-loop' to look at length of variable array above first */
 //for (let i=0; i < pokemonList.length; i++)//{
@@ -96,6 +117,9 @@ pokemonRepository.add({name: "Pikachu", height: 1.1 , type: "fire"});
 
 
 
-pokemonList.forEach (function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+// Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
 });
